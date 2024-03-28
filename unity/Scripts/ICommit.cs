5ba4c942657f1ba19cbe079d8045f39b77a109be
John^2 Li <hokage@deva.ltd>
2024-03-29 01:36:14 +0800

    unity scripts

---
+++ unity/Scripts/ICommit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ICommit
+{
+	Int64 modelHash { get; }
+	string commitMessage { get; }
+	PublicKey author { get; }
+	Int64 authorSignature { get; }
+	bool isMaster { get; set; }
+	byte[] getFullModel();
+	bool checkValid();
+	void rebaseToMaster();
+	double getCompressionRatio();
+}
+
+
+interface DeltaModel : ICommit
+{
+	ICommit parentModel { get; }
+	bool isEncrypted { get; }
+	byte[] modelDelta { get; }
+
+}
+
+
+interface MergeModel : ICommit
+{
+	ICommit[] parentModels { get; }
+
+}
+
+public interface PublicKey
+{
+	string key { get; set; }
+}
