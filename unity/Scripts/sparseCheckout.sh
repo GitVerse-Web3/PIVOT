fb0d37061a665728052c196a0386f58a4b05116e
John^2 Li <hokage@deva.ltd>
2024-03-29 11:45:19 +0800

    sparse checkout sh

--- unity/Scripts/sparseCheckout.sh
+++ unity/Scripts/sparseCheckout.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "this file can be put into the /Assets/unity/Scripts/sparseCheckout.sh"
+cd $(dirname $0)
+pwd
 git init
 git remote add origin git@github.com-pivot:GitVerse-Web3/PIVOT.git
 git config core.sparseCheckout true
